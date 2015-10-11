8bcd88bef33f8afd8e916ab55226e5dea8ad9182
monatru3 <monatru3@hotmail.com>
2015-10-11 12:07:18 -0400

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,9 @@
-def display_board(board)
+
+def move(board, position, choice="X")
+
+board[position.to_i-1]=choice
+
+  def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -6,4 +11,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+end
 # code your move method here!
