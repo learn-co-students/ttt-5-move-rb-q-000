87df0d9db2a21d7d463a47280df858aac8bf1e9a
onyoo <roberto9m9@gmail.com>
2015-10-21 16:32:30 +0200

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def move(board, input, typ = "X")
+#to_i converts string to integer as a method (ex: '5'.to_i)
+  input = input.to_i - 1
+  board[input] = typ
+end
 # code your move method here!
