0e14d71f10369a58a52ea093cbdad6cec03f6609
Julia9CA <julia.hoffman@blueyonder.co.uk>
2015-10-22 18:36:20 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your move method here!
+# code your move method here
+def move(board, position, player = "X")
+  position = position.to_i - 1
+  board[position] = player
+end
+
