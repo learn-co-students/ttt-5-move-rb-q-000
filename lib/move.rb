923b06fac510305a864f5beed75e2fb80bb30716
neonwires <neonwires@gmail.com>
2015-10-27 22:16:47 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,8 @@ def display_board(board)
 end
 
 # code your move method here!
+def move(board, movechoice, playertoken = "X")
+  movechoice = movechoice.to_i
+  movechoice = movechoice - 1
+  board [movechoice] = playertoken
+end
