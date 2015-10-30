34012bc581e6f3a24a4964cba73751f67b6e34e7
RodAquino <rodrigo_59@hotmail.com>
2015-10-30 01:57:06 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,23 +1,27 @@
 
 
-
-
-
-
-
-
 def display_board(board)
-  puts "  #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "  #{board[0]} | #{board[1]} | #{board[2]} "
   puts " -----------"
   puts "  #{board[3]} | #{board[4]} | #{board[5]} "
   puts " -----------"
   puts "  #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move (board,input,"X")
+def move (board,input,char="X")
 
-  board [position] = symbol
+input = input.to_i
+  input -=1
+ board [input] = char
+
+  display_board(board)
+
 end
+
+
+
+
+
 
 
 
