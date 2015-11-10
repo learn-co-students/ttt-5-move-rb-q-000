b1a632815363b80a4039aeda6d197e054b9dfec5
aletamarie <aleta.marie3@gmail.com>
2015-11-10 15:27:13 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,23 @@
-def display_board(board)
+def display_board(board=[" "," "," "," "," "," "," "," "," "])
+
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
 end
+
+display_board([" "," "," "," "," "," "," "," "," "])
+
+def move(board, location, player="X")
+  board[location.to_i-1]=player
+  board=Array.new(9,"")
+
+end
+
+
+
+
 
 # code your move method here!
