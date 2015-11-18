8a09493397ccc170bff5c51614363675d7a74922
bubbaxen <rbind71@yahoo.com>
2015-11-18 16:59:55 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,19 @@ def display_board(board)
 end
 
 # code your move method here!
+
+board = Array.new(9," ")
+# switched choice to 15 for testing default board
+choice = 15
+char = "X"
+
+
+def move(board, choice ,char = "X")
+
+  board[choice.to_i-1] = char
+  display_board(board)
+
+end
+
+# switched char default to "O" for testing default board
+move(board, choice, char)
