434143df86588628f797d5d3aad0d0233981c10f
jenjiyi <jenny.yamada@gmail.com>
2015-11-28 16:11:28 -0800

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,10 @@ def display_board(board)
 end
 
 # code your move method here!
+def move (board, input, char = "X")
+  position = input.to_i - 1
+  def update_array_at_with(array, position, char)
+    array[position] = char
+  end
+  update_array_at_with(board, position, char)
+end
