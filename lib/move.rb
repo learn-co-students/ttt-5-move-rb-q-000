f2e34243b2dac3a811768170f3cce1db8e07ab1e
Ziv Zamechek <ziv921@yahoo.com>
2015-12-16 18:53:28 -0500

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,14 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts "-----------"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # code your move method here!
+def move(board, location, player = "X")
+ puts "Welcome to Tic Tac Toe!"
+ puts "Where would you like to go?"
+ board[(location.to_i)-1] = player
+end
