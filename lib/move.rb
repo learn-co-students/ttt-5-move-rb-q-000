a2908876efa81a2eb39da716f30a9e36b4247386
Wallace Donegan <Wallace.Donegan@gmail.com>
2016-01-01 18:36:01 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def move(array,pos,player="X")
+  array[pos.to_i - 1] = player
+end
+
 # code your move method here!
