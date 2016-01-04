e54fd05d0960a057cbf467c1dda16093a104845b
nscarlyon <natasha.carlyon@gmail.com>
2016-01-04 20:55:10 +0000

    nitrous-backup at Mon Jan  4 20:55:10 UTC 2016

--- lib/move.rb
+++ lib/move.rb
@@ -11,22 +11,9 @@ end
 
 def move(board, position, character = "X")
 
-  #puts "Where would you like to move?"
-  #position = gets.to_i
+
   board[position.to_i-1] = character
   return board
-
-
-  #board[1] = position[2]
-  #board[2] = position[3]
-  #board[3] = position[4]
-  #board[4] = position[5]
- # board[5] = position[6]
- # board[6] = position[7]
- # board[7] = position[8]
- # board[8] = position[9]
-
-
 
 end
 
