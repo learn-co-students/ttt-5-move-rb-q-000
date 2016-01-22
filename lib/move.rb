f7d77b002bbfb23df8bab0337780dcbe9369cee1
Nicholas-Maxwell <nick.maxwell18@gmail.com>
2016-01-22 13:51:05 -0500

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,48 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your move method here!
+    
+def display_board(board)    
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "    
+  puts "-----------"
+end 
+     
+ def current_player(board)   
+   turn_count(board) % 2 == 0 ? "X" : "O"    
+ end   
+     
+ def turn_count(board)   
+   board.count{|token| token == "X" || token == "O"}   
+ end   
+     
+  def move(board, location, player = "X")   
+    board[location.to_i-1] = player   
+  end   
+      
+  def winner(board)   
+   if winning_combo = won?(board)    
+     board[winning_combo.first]    
+  end   
+ end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
