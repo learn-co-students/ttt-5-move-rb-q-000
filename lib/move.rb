a9c7e28953e84555aee14c39d8e3af1cb9429d47
kade042 <dereje88@gmail.com>
2016-02-05 19:10:54 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,9 @@ def display_board(board)
 end
 
 # code your move method here!
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def move(board, postion, value = "X" )
+
+  #postion = postion - 1
+  board[postion.to_i - 1] = value
+end
