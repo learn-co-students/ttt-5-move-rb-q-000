8573ec98626e3fcfc24eed46963ed299c1f04aee
vandepool <contact.vandepool@gmail.com>
2016-02-15 18:09:47 +0000

    nitrous-backup at Mon Feb 15 18:09:47 UTC 2016

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,8 @@ def display_board(board)
 end
 
 # code your move method here!
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def move(array, position, char)
+  array[position] = char
+end
