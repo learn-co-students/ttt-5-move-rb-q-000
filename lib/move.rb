cd754aed4e57af2651e51bf75100335a9bd75fe1
s0uth <abysudario@gmail.com>
2016-02-24 22:13:58 -0800

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,13 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[1]} | #{board[2]} | #{board[3]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{board[4]} | #{board[5]} | #{board[6]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
 end
 
 # code your move method here!
+def move(board, location, player)
+    board[location.to_i] = player
+    return board
+end
