f877487d03c17b23ddf1511e19a4fa850f1d0b90
bradleyoashburn <bradleyoashburn@gmail.com>
2016-02-25 14:45:10 -1000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+#displays the board
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,7 +7,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your move method here!
+#places the player's token in the position entered
+#board = an array
+#position = a string from user input
+#character = an 'X' or 'O'
 def move(board, position = 1, character = 'X')
+  #need to convert user entered string into an integer
+  #need to account for array index starting at 0
   board[position.to_i - 1] = character
 end
