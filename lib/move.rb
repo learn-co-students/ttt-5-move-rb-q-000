9b603baa2f4a514b87ca6d8e09e7470f4f6eb566
Versallly <universallly@gmail.com>
2016-03-23 16:44:47 +0000

    nitrous-backup at Wed Mar 23 16:44:47 UTC 2016

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def move(boards, input, n="X")
-  input = input.to_i
-  boards[input-1] = n
+  input = input.to_i-1
+  boards[input] = n
 end
 # code your move method here!
