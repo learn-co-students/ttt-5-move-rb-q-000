e122538e46a2659a29edeca6bf979507d41d72c5
bging4 <wgingher@gmail.com>
2016-04-11 11:52:08 -0400

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, input, player="X")
-  board[input.to_i - 1] = player
+def move(board, input, current_player="X")
+  board[input.to_i - 1] = current_player
 end
-
