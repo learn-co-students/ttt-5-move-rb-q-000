28f0dd639a4f2e2585fb7017164c7e735f8d99c1
klovae <elyse.klova@gmail.com>
2016-04-13 18:38:45 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,13 @@
 def display_board(board)
+  line = "-----------"
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
+  puts line
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
+  puts line
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 # code your move method here!
+def move(board, input, char = "X")
+    board[input.to_i - 1] = char
+end
