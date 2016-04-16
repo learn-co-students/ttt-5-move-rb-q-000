b8e27c39df0eb28a76e89fc4bd8d55c997d92afa
rikari0813 <christiane.maico@gmail.com>
2016-04-16 11:01:12 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your move method here!
+def move(board,input,marker = "X")
+  board[input.to_i - 1] = marker
+  puts display_board(board)
+end
