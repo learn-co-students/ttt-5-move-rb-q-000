eadc3bfa9dab8f3eb77f09d71baf1292c09f351d
kevin paras <kevincparas@gmail.com>
2016-05-13 21:41:44 -0700

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,19 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+board = [" ", " "," "," "," "," "," "," "," ",]
+
+def move(array, position, player)
+  puts "X or O?"
+  player = gets.strip
+  puts "Where would you like to go?"
+  puts "#{display_board(board)}"
+  response = gets.to_i.strip
+  position = response - 1
+  array[position] = player
+  return "#{display_board(board)}"
+
+
+end
+
 # code your move method here!
