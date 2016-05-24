f9cf46fe41f9c75568aa50b43a13c2fc570e92d7
AlicT <alic.trossman@gmail.com>
2016-05-24 12:34:30 -0400

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,8 +7,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(users_input)
-  index = users_input.to_i - 1
+def input_to_index(input)
+  index = input.to_i - 1
 end
 
 def move(board, index, token = "X")
