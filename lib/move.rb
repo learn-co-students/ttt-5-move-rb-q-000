07ca833aad47c6df178ea79069181f5ac190c098
David H. Friedman <dhf@panix.com>
2016-05-25 08:59:23 -0400

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,18 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(input)
+  Integer(input)-1
+rescue ArgumentError
+  -1
+end
+
+def move(board, where, what="X")
+  if what != "X" and what != "O"
+    raise ArgumentError, "#move wants `X` or `O` got `#{what}`"
+  end
+  if where < 0 or where > 8
+    raise ArgumentError, "#move wants position in [0,8] got `#{where}`"
+  end
+  board[where] = what
+end
