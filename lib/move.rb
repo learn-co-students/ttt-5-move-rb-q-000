89953e17736bab1a856e32cd1c4b5a475239a0e1
Sam Papke <skpapke@gmail.com>
2016-06-30 02:48:16 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,22 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(index)
+  index.to_i - 1
+end
+
+def move (board, index, value = "X")
+  board[index] = value
+end
+
+def validate_move(index, board)
+  if index > 9
+    puts "Invalid input. Enter a number between 1 and 9."
+    return false
+  end
+  if board[index] == "X" || board[index] == "O"
+    puts "Invalid input. Select an empty space."
+    return false
+  end
+  return true
+end
