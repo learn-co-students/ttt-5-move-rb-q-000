0bddc22da1c0bbecd7683ca674f0f77e768c8417
smithwebtek <brad@smithwebtek.com>
2017-09-04 20:43:12 -0400

    minor refactor

--- lib/move.rb
+++ lib/move.rb
@@ -1,5 +1,3 @@
-
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -8,6 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, position, value = "X")
-  board[(position.to_i)-1] = value
+def move(board, position, token = "X")
+	board[(position.to_i)-1] = token
 end
