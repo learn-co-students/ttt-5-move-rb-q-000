8b0cc67519d868fd39fc969677340b5759d30b90
Amy Carney <amy.carney@alaska.gov>
2017-12-13 18:26:57 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -8,9 +8,9 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(input)
-  input.to_i
+  input.to_i - 1
 end
 
-def move(array, index, value)
-  array[index] = value
+def move(array, input, value="X")
+  array[input] = value
 end
