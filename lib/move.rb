3d66c6416935f59a00df52a20ae14d5cbbb78e8c
smithwebtek <brad@smithwebtek.com>
2018-05-04 01:20:23 -0400

    Update my lab to latest

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,6 +7,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, position, token = "X")
-	board[(position.to_i)-1] = token
+# code your input_to_index and move method here!
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
